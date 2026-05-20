91e5a139f3fdd2122b64046d93f75348a78245ef
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2026-05-20 17:19:41 +0530

    modified outputs

--- outputs.tf
+++ outputs.tf
@@ -4,10 +4,3 @@ output "resource_group_names" {
     k => rg.resource_group_name
   }
 }
-
-output "resource_group_ids" {
-  value = {
-    for k, rg in module.resource_groups :
-    k => rg.resource_group_id
-  }
-}
