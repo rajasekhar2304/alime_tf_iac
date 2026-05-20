5bb3b98f3af5fed3bdaa1f997e2f9d604db36bf6
root <root@terraform-host.a1pnvdim1tpe1c2feuej0d4yid.rx.internal.cloudapp.net>
2026-05-20 11:09:37 +0000

    applied tf format

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "resource_groups" {
   for_each    = var.resource_groups
   rg_name     = each.value.rg_name
   rg_location = each.value.rg_location
-  tags        = merge(
+  tags = merge(
     local.common_tags,
     each.value.tags
   )
