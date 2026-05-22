cd684c30db068f9d6898ced5c0a1b92b947b4442
root <root@terraform-host.a1pnvdim1tpe1c2feuej0d4yid.rx.internal.cloudapp.net>
2026-05-22 04:16:58 +0000

    applied tf fmt

--- modules/firewall-nat-rule/main.tf
+++ modules/firewall-nat-rule/main.tf
@@ -1,21 +1,21 @@
 resource "azurerm_firewall_nat_rule_collection" "nat_rule" {
-  name = var.name
+  name                = var.name
   azure_firewall_name = var.firewall_name
   resource_group_name = var.resource_group_name
-  priority = var.priority
-  action = var.action
+  priority            = var.priority
+  action              = var.action
   dynamic "rule" {
     for_each = var.rules
     content {
-      name = rule.value.name
-      source_addresses = rule.value.source_addresses
+      name              = rule.value.name
+      source_addresses  = rule.value.source_addresses
       destination_ports = rule.value.destination_ports
       destination_addresses = [
         var.firewall_public_ip
       ]
       translated_address = rule.value.translated_address
-      translated_port = rule.value.translated_port
-      protocols = rule.value.protocols
+      translated_port    = rule.value.translated_port
+      protocols          = rule.value.protocols
     }
   }
 }
